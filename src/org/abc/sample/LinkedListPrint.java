c3155a78fc84c56208c9214c41199b17371041dc
anitha <anishakthi@gmail.com>
2017-03-25 18:29:22 +0530

    removed using of list in building a linked list.

--- src/org/abc/sample/LinkedListPrint.java
+++ src/org/abc/sample/LinkedListPrint.java
@@ -1,54 +1,48 @@
 package org.abc.sample;
 
-import java.util.List;
-import java.util.ArrayList;
-
 public class LinkedListPrint {
 	
 	public static void main(String[] args) {
-		
+
 		// constructing a linked list
-		List<Node> linkedList = new ArrayList<Node>();
 		// 10 indicate the size of the list and also the data in the node is added from 1 to 10.
-		buildLinkedList(linkedList, null, 10);
-		
+		Node firstNode = buildLinkedList(null, 10);
+
 		// Printing the linked list
-		System.out.print("Linked List is " + linkedList.get(linkedList.size() -1).getData()); // got last value from the list as the first element is inserted as last element
-		for(int i=1; i<linkedList.size() ; i++) {
-			Node node = linkedList.get(linkedList.size()-i);
-			System.out.print(" -> " + getNextNode(node));
+		System.out.print("Linked List is " + firstNode.getData());
+
+		for (int i = 1; i < 10; i++) {
+			Node nextNode = getNextNodes(firstNode);
+			System.out.print(" -> " + nextNode.getData());
+			firstNode = nextNode;
 		}
- 	}
+	}
 	
 	/**
 	 * Building a Linked list with incrementing the value from 1 to 10
 	 * @param linkedList - The list in which Node is added.
 	 * @param nextNode - the next node.
 	 * @param data - input data. This also indicate the size of the list.
-	 * @return
+	 * @return First node
 	 */
-	private static List<Node> buildLinkedList(List<Node> linkedList, Node nextNode, int data) {
-			if(data <= 0) {
-			return linkedList; 
-			} else {
-			Node node = new Node();
-			node.setData(data);
-			node.setNext(nextNode);
-			linkedList.add(node);
-			return buildLinkedList(linkedList, node, data-1);
-			}
-			
+	private static Node buildLinkedList(Node nextNode, int data) {
+		if (data == 0) {
+			return nextNode;
+		}
+		Node node = new Node();
+		node.setData(data);
+		node.setNext(nextNode);
+		return buildLinkedList(node, data - 1);
 	}
 
 	/**
-	 * Method to retrieve the next element's data of the list
+	 * Method to retrieve the next node of the list
 	 * @param node - Current Node.
-	 * @return the data of the next node.
+	 * @return the next node.
 	 */
-	private static int getNextNode(Node node) {
-		return node.getNext().getData();
+	private static Node getNextNodes(Node node) {
+		return node.getNext();
 	}
-	
 }
 
 
